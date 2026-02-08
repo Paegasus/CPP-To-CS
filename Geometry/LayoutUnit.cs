d2eac728cde41858c7eb4fcb99b6d05b019114ef
Paegasus <lahcenebelbachir@gmail.com>
2026-02-08 19:52:29 +0100

    Changes.

--- Geometry/LayoutUnit.cs
+++ Geometry/LayoutUnit.cs
@@ -4,6 +4,23 @@ using UI.Numerics;
 
 namespace UI.Geometry;
 
+// LayoutUnit implements the IFixedPoint interface,
+// storing multiples of 1/64 of a pixel in an int (System.Int32).
+//
+// LayoutUnit is a subpixel unit used by the rendering tree,
+// to represent locations and sizes instead of integers,
+// which results in bette zooming support (both on desktop and mobile).
+//
+// LayoutUnit is an abstraction used to represent the location or size of a render object in fractions of a logical pixel,
+// it is used primarily for layout and hit testing.
+// The current implementation represents values as multiples of 1/64th pixel.
+// This allows us to use integer math and avoids floating point imprecision.
+// Even though layout calculations are done using LayoutUnits
+// the values are aligned to integer pixel values at paint time to line up with device pixels.
+// While most modern graphics libraries support painting with subpixel precision,
+// this results in unwanted anti-aliasing.
+// When aligning to device pixels the edges are aligned to the nearest pixel and then the size is adjusted accordingly.
+// This ensures that the bottom/right edge and the total width/height is at most off-by-one.
 public struct LayoutUnit : IFixedPoint<int, uint>
 {
 	public const int FractionalBits = 6;
@@ -65,8 +82,27 @@ public struct LayoutUnit : IFixedPoint<int, uint>
 		return (double)m_Value / FixedPointDenominator;
 	}
 
-	public readonly int ClampRawValue<T>(T rawValue) where T : IBinaryInteger<T>
+	// Note: Original C++ has this function here as well, but since it's static and only calls Conversion.SaturatedCast(),
+	// I'm not sure if we should keep it in LayoutUnit, there might be a better place for it.
+	public static int ClampRawValue<T>(T value) where T : IBinaryInteger<T>
 	{
-        return Conversion.SaturatedCast<int, T>(rawValue);
+        return Conversion.SaturatedCast<int, T>(value);
+	}
+
+	public static LayoutUnit FromRawValue(int value)
+	{
+		LayoutUnit unit = new()
+		{
+			m_Value = value
+		};
+		
+		return unit;
+	}
+	
+	public static LayoutUnit FromRawValueWithClamp<T>(T raw_value) where T : IBinaryInteger<T>
+	{
+		// Note: Might be better to just call Conversion.SaturatedCast() directly and avoid the indirect call to ClampRawValue()
+		// return FromRawValue(Conversion.SaturatedCast<int, T>(raw_value));
+		return FromRawValue(LayoutUnit.ClampRawValue<T>(raw_value));
 	}
 }
