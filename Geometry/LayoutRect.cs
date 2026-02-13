b7e82bd72c2e409d528d8790a82d57e12ff5823a
Paegasus <lahcenebelbachir@gmail.com>
2026-02-13 02:53:30 +0100

    Changes.

--- Geometry/LayoutRect.cs
+++ Geometry/LayoutRect.cs
@@ -7,16 +7,18 @@ public struct LayoutRect
     private LayoutPoint m_Location;
     private LayoutSize m_Size;
 
-    public LayoutUnit X { readonly get => m_Location.X; set => m_Location.X = value; }
-    public LayoutUnit Y { readonly get => m_Location.Y; set => m_Location.Y = value; }
-    public readonly LayoutUnit MaxX => X + Width;
-    public readonly LayoutUnit MaxY => Y + Height;
-    public readonly LayoutUnit Width => m_Size.Width;
-    public readonly LayoutUnit Height => m_Size.Height;
-
     public readonly LayoutPoint Location => m_Location;
     public readonly LayoutSize Size => m_Size;
 
+    public LayoutUnit X { readonly get => m_Location.X; set => m_Location.X = value; }
+    public LayoutUnit Y { readonly get => m_Location.Y; set => m_Location.Y = value; }
+
+    public LayoutUnit Width { readonly get => m_Size.Width; set => m_Size.Width = value; }
+    public LayoutUnit Height { readonly get => m_Size.Height; set => m_Size.Height = value; }
+
+    public readonly LayoutUnit MaxX => X + Width;
+    public readonly LayoutUnit MaxY => Y + Height;
+    
     public LayoutRect(in IntRect rect)
     {
         m_Location = rect.Location;
