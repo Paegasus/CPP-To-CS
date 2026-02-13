8c77ba64fda50a84bc41d45a6b5cd63914bd4abb
Paegasus <lahcenebelbachir@gmail.com>
2026-02-13 19:09:43 +0100

    Point.cs work.

--- GFX/Geometry/Point.cs
+++ GFX/Geometry/Point.cs
@@ -1,9 +1,34 @@
 namespace UI.GFX.Geometry;
 
+// A point has an x and y coordinate.
 public struct Point
 {
-    private int m_X, m_Y;
+    private int x_, y_; // Rename to m_X and m_Y
 
-    public int X { readonly get => m_X; set => m_X = value; }
-    public int Y { readonly get => m_Y; set => m_Y = value; }
+    public int x { readonly get => x_; set => x_ = value; }
+    public int y { readonly get => y_; set => y_ = value; }
+
+    public Point()
+    {
+        x_ = 0;
+        y_ = 0;
+    }
+    
+    public Point(int x, int y)
+    {
+        x_ = x;
+        y_ = y;
+    }
+
+    public void SetPoint(int x, int y)
+    {
+        x_ = x;
+        y_ = y;
+    }
+
+    void Offset(int delta_x, int delta_y)
+    {
+        //x_ = base::ClampAdd(x_, delta_x);
+        //y_ = base::ClampAdd(y_, delta_y);
+    }
 }
