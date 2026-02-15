e11743922ab64ea81b71f04f439e8dcdbd1fa7c0
gamedevionist <gamedevionist@gmail.com>
2026-02-15 13:22:53 +0000

    Created Insets.

---
+++ GFX/Geometry/Insets.cs
@@ -0,0 +1,208 @@
+using System;
+using UI.Numerics;
+using static UI.Numerics.ClampedMath;
+
+namespace UI.GFX.Geometry;
+
+/// <summary>
+/// This can be used to represent a space within a rectangle, by "shrinking" the
+/// rectangle by the inset amount on all four sides. Alternatively, it can
+/// represent a border that has a different thickness on each side.
+/// </summary>
+public struct Insets : IEquatable<Insets>
+{
+    private int top_;
+    private int left_;
+    private int bottom_;
+    private int right_;
+
+    public int top() => top_;
+    public int left() => left_;
+    public int bottom() => bottom_;
+    public int right() => right_;
+
+    public Insets(int all)
+    {
+        top_ = all;
+        left_ = all;
+        bottom_ = ClampBottomOrRight(all, all);
+        right_ = ClampBottomOrRight(all, all);
+    }
+
+    public Insets(int vertical, int horizontal)
+    {
+        top_ = vertical;
+        left_ = horizontal;
+        bottom_ = ClampBottomOrRight(vertical, vertical);
+        right_ = ClampBottomOrRight(horizontal, horizontal);
+    }
+    
+    public Insets(int top, int left, int bottom, int right)
+    {
+        top_ = top;
+        left_ = left;
+        bottom_ = ClampBottomOrRight(top, bottom);
+        right_ = ClampBottomOrRight(left, right);
+    }
+
+    /// <summary>
+    /// Returns the total width taken up by the insets.
+    /// </summary>
+    public readonly int width() => left_ + right_;
+
+    /// <summary>
+    /// Returns the total height taken up by the insets.
+    /// </summary>
+    public readonly int height() => top_ + bottom_;
+
+    /// <summary>
+    /// Returns the size of the insets.
+    /// </summary>
+    public readonly Size size() => new Size(width(), height());
+
+    /// <summary>
+    /// Returns true if the insets are empty.
+    /// </summary>
+    public readonly bool IsEmpty() => width() == 0 && height() == 0;
+    
+    /// <summary>
+    /// Flips x- and y-axes.
+    /// </summary>
+    public void Transpose()
+    {
+        (top_, left_) = (left_, top_);
+        (bottom_, right_) = (right_, bottom_);
+    }
+
+    public Insets set_top(int top)
+    {
+        top_ = top;
+        bottom_ = ClampBottomOrRight(top_, bottom_);
+        return this;
+    }
+
+    public Insets set_left(int left)
+    {
+        left_ = left;
+        right_ = ClampBottomOrRight(left_, right_);
+        return this;
+    }
+
+    public Insets set_bottom(int bottom)
+    {
+        bottom_ = ClampBottomOrRight(top_, bottom);
+        return this;
+    }
+
+    public Insets set_right(int right)
+    {
+        right_ = ClampBottomOrRight(left_, right);
+        return this;
+    }
+    
+    public Insets set_left_right(int left, int right) {
+        left_ = left;
+        right_ = ClampBottomOrRight(left_, right);
+        return this;
+    }
+
+    public Insets set_top_bottom(int top, int bottom) {
+        top_ = top;
+        bottom_ = ClampBottomOrRight(top_, bottom);
+        return this;
+    }
+
+    public static Insets TLBR(int top, int left, int bottom, int right)
+    {
+        return new Insets(top, left, bottom, right);
+    }
+
+    public static Insets VH(int vertical, int horizontal)
+    {
+        return new Insets(vertical, horizontal);
+    }
+    
+    public void SetToMax(in Insets other) {
+        top_ = Math.Max(top_, other.top_);
+        left_ = Math.Max(left_, other.left_);
+        bottom_ = Math.Max(bottom_, other.bottom_);
+        right_ = Math.Max(right_, other.right_);
+    }
+
+    // Conversion from Insets to Outsets negates all components.
+    // public Outsets ToOutsets() => new Outsets(-top(), -left(), -bottom(), -right());
+    
+    /// <summary>
+    /// Adjusts the vertical and horizontal dimensions by the values described in |vector|.
+    /// Offsetting insets before applying to a rectangle would be equivalent to offsetting
+    /// the rectangle then applying the insets.
+    /// </summary>
+    public void Offset(in Vector2D vector)
+    {
+        set_left_right(ClampAdd(left(), vector.x), ClampSub(right(), vector.x));
+        set_top_bottom(ClampAdd(top(), vector.y), ClampSub(bottom(), vector.y));
+    }
+    
+    // public static explicit operator InsetsF(in Insets i) => new InsetsF(i);
+
+    public override readonly string ToString() => $"x:{left_},{right_} y:{top_},{bottom_}";
+
+    public override readonly int GetHashCode() => HashCode.Combine(top_, left_, bottom_, right_);
+
+    public override readonly bool Equals(object? obj) => obj is Insets other && Equals(other);
+
+    public readonly bool Equals(Insets other) =>
+        top_ == other.top_ && left_ == other.left_ && bottom_ == other.bottom_ && right_ == other.right_;
+    
+    public static bool operator ==(in Insets left, in Insets right) => left.Equals(right);
+    public static bool operator !=(in Insets left, in Insets right) => !left.Equals(right);
+
+    public void operator +=(in Insets other)
+    {
+        top_ = ClampAdd(top_, other.top_);
+        left_ = ClampAdd(left_, other.left_);
+        bottom_ = ClampBottomOrRight(top_, ClampAdd(bottom_, other.bottom_));
+        right_ = ClampBottomOrRight(left_, ClampAdd(right_, other.right_));
+    }
+
+    public void operator -=(in Insets other)
+    {
+        top_ = ClampSub(top_, other.top_);
+        left_ = ClampSub(left_, other.left_);
+        bottom_ = ClampBottomOrRight(top_, ClampSub(bottom_, other.bottom_));
+        right_ = ClampBottomOrRight(left_, ClampSub(right_, other.right_));
+    }
+
+    public static Insets operator -(in Insets v)
+    {
+        return new Insets(
+            -v.top(),
+            -v.left(),
+            -v.bottom(),
+            -v.right()
+        );
+    }
+    
+    public static Insets operator +(Insets lhs, in Insets rhs)
+    {
+        lhs += rhs;
+        return lhs;
+    }
+    
+    public static Insets operator -(Insets lhs, in Insets rhs)
+    {
+        lhs -= rhs;
+        return lhs;
+    }
+    
+    public static Insets operator +(Insets insets, in Vector2D offset)
+    {
+        insets.Offset(offset);
+        return insets;
+    }
+    
+    private static int ClampBottomOrRight(int top_or_left, int bottom_or_right)
+    {
+        return ClampAdd(top_or_left, bottom_or_right) - top_or_left;
+    }
+}
